267eddbef3ef0f3f70edf43937690db6a739086a
minhyeong7 <duswlsalsgud@naver.com>
2025-04-26 22:02:28 +0900

    README.md 수정

--- src/service/header.js
+++ src/service/header.js
@@ -1,7 +1,7 @@
 // API호출
 //export const server = "http://172.20.10.6:8000/api"; // 핫스팟 서버 주소
 //export const server = "http://192.168.192.2:8000/api"; // zerotier 서버 주소
-export const server = "http://192.168.0.145:8000/api"; // CCIT 서버 주소
+export const server = "http://192.168.0.1451:8000/api"; // CCIT 서버 주소
 //export const server = "http://10.80.13.56:8000/api"; // JBU-WLAN 서버 주소
 
 
@@ -9,9 +9,7 @@ export const server = "http://192.168.0.145:8000/api"; // CCIT 서버 주소
 export const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
 //export const wsHost = '172.20.10.6:8000'; // 핫스팟 서버 주소
 //export const wsHost = '192.168.192.2:8000'; // zerotier 서버 주소
-export const wsHost = '192.168.0.145:8000'; // CCIT 서버 주소
-//export const wsHost = '10.80.13.56:8000'; // JBU-WLAN 서버 주소
+export const wsHost = '192.168.01.145:8000'; // CCIT 서버 주소
+//export const wsHost = '10.80.13.56:8000'; // JBU-WL
 
-// git pull origin main
 
-// 임동균
