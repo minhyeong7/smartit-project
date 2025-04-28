37f803dd9ab5ebfad69019542d548e3df90494bd
asdniofo <ceyeun33@jmail.ac.kr>
2025-04-28 15:32:37 +0900

    날씨url+header세팅 바꿈꿈

--- src/service/header.js
+++ src/service/header.js
@@ -1,17 +1,13 @@
-// API호출
-//export const server = "http://172.20.10.6:8000/api"; // 핫스팟 서버 주소
-//export const server = "http://192.168.192.2:8000/api"; // zerotier 서버 주소
-export const server = "http://192.168.0.145:8000/api"; // CCIT 서버 주소
-//export const server = "http://10.80.13.56:8000/api"; // JBU-WLAN 서버 주소
+// 서버 설정
+//const SERVER_IP = 'localhost:8000'; // 로컬 주소
+//const SERVER_IP = '172.20.10.6:8000'; // 핫스팟 서버 주소
+//const SERVER_IP = '192.168.192.2:8000'; // zerotier 서버 주소
+const SERVER_IP = '192.168.0.145:8000'; // CCIT 서버 주소
+//const SERVER_IP = '10.80.13.56:8000'; // JBU-WLAN 서버 주소
 
+// API 호출 URL 생성
+export const server = `http://${SERVER_IP}/api`;
 
-// 웹소켓
+// 웹소켓 설정
 export const wsProtocol = window.location.protocol === 'https:' ? 'wss://' : 'ws://';
-//export const wsHost = '172.20.10.6:8000'; // 핫스팟 서버 주소
-//export const wsHost = '192.168.192.2:8000'; // zerotier 서버 주소
-export const wsHost = '192.168.0.145:8000'; // CCIT 서버 주소
-//export const wsHost = '10.80.13.56:8000'; // JBU-WLAN 서버 주소
-
-// git pull origin main
-
-// 임동균
+export const wsHost = SERVER_IP;
